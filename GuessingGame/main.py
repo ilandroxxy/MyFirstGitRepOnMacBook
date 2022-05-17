ff79a090c2ee96677883d3ec0c6ca0a7c80c21e2
Ilya Andrianov <ilyand98@mail.ru>
2022-05-17 19:02:05 +0700

    Added score for the game

--- GuessingGame/main.py
+++ GuessingGame/main.py
@@ -14,17 +14,19 @@ print("\ncase 1: Start game\ncase 0: Exit game\n")
 case = input("case: ")
 
 while True:
-
     if case == '2':
         case = '1'
 
     if case == '1':
         print("Игра началась, угадывайте число!")
+        score = 0
         key = random.randint(1, 100)
         x = int(input("Введите свое число: "))
+        score += 1
         while is_status(x, key) != True:
             x = int(input("Введите свое число: "))
-        print("You win!")
+            score += 1
+        print("You win!\nYour Score:", score)
 
 
     if case == '0':
