6002a59e979287276a863bb3b48e1ecdcc64e8c9
Ilya Andrianov <ilyand98@mail.ru>
2022-05-17 18:47:54 +0700

    Add structur for my game

---
+++ GuessingGame/main.py
@@ -0,0 +1,18 @@
+
+
+print("Добро пожаловать в игру 'Угадай число', начнем играть!")
+
+
+while True:
+    print("\ncase 1: Start game\ncase 2: Restart game\ncase 0: Exit game\n")
+    case = input("case: ")
+
+    if case == '1':
+        pass
+
+    if case == '2':
+        pass
+
+    if case == '0':
+        print("Спасибо за проведенную игру, Bye!")
+        break
