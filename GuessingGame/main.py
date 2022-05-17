fd89ceef57c9e8687fd6d9cf3172be07e579f3b7
Ilya Andrianov <ilyand98@mail.ru>
2022-05-17 18:56:43 +0700

    Start do our game logic

--- GuessingGame/main.py
+++ GuessingGame/main.py
@@ -1,14 +1,27 @@
+import random
+
+def is_status(x, key):
+    if x == key:
+        return True
+    if x < key:
+        print("Число слишком мало, возьмите больше.")
+    if x > key:
+        print("Число большое, берите меньше.")
 
 
 print("Добро пожаловать в игру 'Угадай число', начнем играть!")
-
 
 while True:
     print("\ncase 1: Start game\ncase 2: Restart game\ncase 0: Exit game\n")
     case = input("case: ")
 
     if case == '1':
-        pass
+        key = random.randint(1, 100)
+        x = int(input("Введите свое число: "))
+        while is_status(x, key) != True:
+            x = int(input("Введите свое число: "))
+        print("You win!")
+
 
     if case == '2':
         pass
